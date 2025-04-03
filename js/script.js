31b323cfa021d0cf1b291d89db92f472a4387db3
Isaaq Simon <IsaaqS>
2025-04-03 15:14:37 +0000

    Hexagon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+function calculateHexagon() {
+    // Get the side length from the input.
+    let sideLength = parseFloat(document.getElementById("sideLength").value);
+
+    // Calculate the perimeter.
+    let perimeter = sideLength * 6;
+
+    // Calculate the area.
+    let area = (3 * Math.sqrt(3) / 2) * Math.pow(sideLength, 2);
+
+    // Round the results to 2 decimal places.
+    perimeter = perimeter.toFixed(2);
+    area = area.toFixed(2);
+
+    // Display the results.
+    document.getElementById("result").innerHTML = "Perimeter: " + perimeter + " cm, Area: " + area + " cm²";
+}
